c70cbfcecfd24ef767d9317533164ec8c1e3264d
mosapeizer <k471352@gmail.com>
2019-07-19 10:19:46 +0800

    add random string

--- test/promise.test.js
+++ test/promise.test.js
@@ -1,7 +1,8 @@
+const randomstring = require('randomstring')
 const MyPromise = require('@/promise');
 
 test('can resolve primitive value', () => {
-  const expectTo = 123
+  const expectTo = randomstring.generate(7)
   const p = new MyPromise((resolve) => resolve(expectTo));
   return p.then(value => expect(value).toBe(expectTo))
 });
